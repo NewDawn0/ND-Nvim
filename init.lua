ad0542744c4f6ae26942601afa8d32216cc00bce
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2022-10-05 12:25:15 +0200

    added autocmds

--- init.lua
+++ init.lua
@@ -16,6 +16,7 @@ require('settings')
 require('colourschemes')
 require('mappings')
 require('plugins')
+require('autocmd')
 
 -- /////// Setup ////////
 cmd("colorscheme onedark")
---
+++ lua/autocmd/init.lua
@@ -0,0 +1,19 @@
+local autocmd = vim.api.nvim_create_autocmd
+local api = vim.api
+local cmd = vim.cmd
+
+-- dont list quickfix buffers
+autocmd("FileType", {
+  pattern = "qf",
+  callback = function()
+    vim.opt_local.buflisted = false
+  end,
+})
+
+-- Run MarkdownPreviewToggle when opening Markdown files
+cmd("autocmd BufEnter *.md | MarkdownPreviewToggle")
+
+-- Turn autosave on for certain filetypes
+cmd("autocmd BufEnter *.md | ASToggle")
+cmd("autocmd BufEnter *.txt | AsToggle")
+cmd("autocmd BufEnter *.tex | AsToggle")
