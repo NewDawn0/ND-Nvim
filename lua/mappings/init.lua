691cdacb44915d499a5fa39baeb88a3735530d39
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2022-10-05 12:24:32 +0200

    updated mappings

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -1,8 +1,10 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-vim.g.mapleader = ' '                                             -- Leader key
-map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)                -- NvimTree
-map('n', '<leader>as', ':ASToggle<CR>', opts)                     -- AutoSave
-map('n', '<leader>tt', ':ToggleTerm direction=horizontal<CR>', opts)  -- ToogleTerm
-map('n', '<leader>tf', ':ToggleTerm<CR>', opts)                       -- ToggleTerm
+vim.g.mapleader = ' '                                                   -- Leader key
+map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)                      -- NvimTree
+map('n', '<leader>as', ':ASToggle<CR>', opts)                           -- AutoSave
+map('n', '<leader>tt', ':ToggleTerm direction=horizontal<CR>', opts)    -- ToogleTerm
+map('n', '<leader>tf', ':ToggleTerm<CR>', opts)                         -- ToggleTerm
+map('n', '<leader>w', ':w<CR>', opts)                                   -- Save
+map('n', '<leader>wq', ':wq<CR>', opts)                                 -- Save and Quit
