84fdcc5b404e504644bcfbd9ad2cf0d4645e40dd
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-03-04 18:50:32 +0100

    improved lsp

--- lua/lsp-config/init.lua
+++ lua/lsp-config/init.lua
@@ -3,15 +3,3 @@ local nvim_lsp = require("lspconfig")
 local on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 end
--- Set flags, capabilities and servers --
-local lsp_flags = { debounce_text_changes = 150 }
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
--- Setup servers --
-for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup {
-        on_attach = require("aerial").on_attach,
-        flags = lsp_flags,
-        capabilities = capabilities
-    }
-end
--- lua/lsp-config/mason-lsp.lua
+++ lua/lsp-config/mason-lsp.lua
@@ -1,6 +1,14 @@
-require("mason-lspconfig").setup({
-    ensure_installed = {
-        "lua_ls", "rust_analyzer", "clangd",
-        "jdtls", "marksman" , "pyright"
-    }
-})
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+
+    -- Default handlers
+    function (server)
+        require("lspconfig")[server].setup {
+            on_attach = require("aerial").on_attach,
+            capabilities = require("cmp_nvim_lsp").default_capabilities()
+        }
+    end,
+
+    -- Dedicated handlers
+}
