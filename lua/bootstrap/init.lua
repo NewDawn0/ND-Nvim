af64c2614ec07f2858525ce29e08ad41e71971bb
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-03-11 22:55:37 +0100

    setup lazy.nvim

---
+++ lua/bootstrap/init.lua
@@ -0,0 +1,21 @@
+--[[
+   _   _ ____        _   ___     ___
+  | \ | |  _ \      | \ | \ \   / (_)_ __ ___
+  |  \| | | | |_____|  \| |\ \ / /| | '_ ` _ \
+  | |\  | |_| |_____| |\  | \ V / | | | | | | |
+  |_| \_|____/      |_| \_|  \_/  |_|_| |_| |_|
+  File: init.lua
+  Desc: Bootstrapping the package manager
+--]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
