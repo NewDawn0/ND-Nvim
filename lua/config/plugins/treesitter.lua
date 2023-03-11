8bd3978efb7dbbf17ab5c973902b292d44190042
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-03-12 00:49:32 +0100

    added treesitter + fixed wrong plugin on last commit

---
+++ lua/config/plugins/treesitter.lua
@@ -0,0 +1,58 @@
+--[[
+   _   _ ____        _   ___     ___
+  | \ | |  _ \      | \ | \ \   / (_)_ __ ___
+  |  \| | | | |_____|  \| |\ \ / /| | '_ ` _ \
+  | |\  | |_| |_____| |\  | \ V / | | | | | | |
+  |_| \_|____/      |_| \_|  \_/  |_|_| |_| |_|
+  File: treesitter.lua
+  Desc: Treesitter language parser config
+--]]
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = {
+        'lua', 'go', 'java',
+        'python', 'c_sharp',
+        'rust', 'javascript',
+        'c', 'cpp', 'asm',
+    },
+    sync_install = false,
+    ignore_install = {},
+    highlight = {
+        enable = true,
+        disable = {},
+        additional_vim_regex_highlighting = false
+    },
+    context_commentstring = {
+        enable = true,
+        enable_autocmd = false
+    },
+    rainbow = {
+        enable = true,
+        extended_mode = false,
+        max_file_lines = nil
+    },
+    autopairs = { enable = true },
+    autotag = { enable = true },
+    incremental_selection = { enable = true },
+    indent = { enable = false },
+    refactor = {
+        highlight_definitions = {
+            enable = true,
+            clear_on_cursor_move = true,
+        },
+        highlight_current_scope = { enable = false },
+        smart_rename = {
+            enable = true,
+            keymaps = { smart_rename = "grr" }
+        },
+        navigation = {
+            enable = true,
+            keymaps = {
+                goto_definition = "gnd",
+                list_definitions = "gnD",
+                list_definitions_toc = "gO",
+                goto_next_usage = "gnn",
+                goto_previous_usage = "gnp"
+            }
+        }
+    }
+}
