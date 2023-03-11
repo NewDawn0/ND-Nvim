0364ac4bb056b76765409152764c3b1f795f15c8
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-03-11 23:03:01 +0100

    added following colourschemes: edge onedark tokyodark

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,4 +12,15 @@ return {
     { 'marko-cerovac/material.nvim', lazy = false, priority = 10,
         config = function() require 'colourschemes.material' end
     },
+    {'sainnhe/edge', lazy = true , priority = 10,
+        config = function() require 'colourschemes.edge' end
+    },
+    {'tiagovla/tokyodark.nvim', lazy = true, priority = 10,
+        config = function() require 'colourschemes.tokyodark' end
+    },
+    { 'folke/tokyonight.nvim', lazy = true, priority = 10 },
+    { 'navarasu/onedark.nvim',
+    	lazy = true, priority = 10,
+        config = function() require 'colourschemes.onedark' end
+    },
 }
