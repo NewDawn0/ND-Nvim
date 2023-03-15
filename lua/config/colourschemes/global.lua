09d6f37d15801a571c433d06a712014f0ceae66e
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-03-15 23:04:18 +0100

    rm unused + add global theme

---
+++ lua/config/colourschemes/global.lua
@@ -0,0 +1,40 @@
+--[[
+   _   _ ____        _   ___     ___
+  | \ | |  _ \      | \ | \ \   / (_)_ __ ___
+  |  \| | | | |_____|  \| |\ \ / /| | '_ ` _ \
+  | |\  | |_| |_____| |\  | \ V / | | | | | | |
+  |_| \_|____/      |_| \_|  \_/  |_|_| |_| |_|
+  File: global.lua
+  Desc: My global custom colours
+--]]
+local colours = {
+    none = "NONE",
+    fg = "#c6cced",
+    fg1 = "#adc3ea",
+    bg = "#0b0d14",
+    bg1 = "#0e0f30",
+    black = "#181a1f",
+    black1 = "#1f1f25",
+    green = "#98be65",
+    green1 = "#87ad54",
+    white = "#ededed",
+    white2 = "#dedede",
+    blue = "#51afef",
+    blue1 = "#7589ee",
+    orange = "#ff9640",
+    orange1 = "#ff8b3d",
+    yellow = "#ffe240",
+    yellow1 = "#fecc27",
+    red = "#ff5370",
+    red2 = '#ec5f67',
+    red3 = "#ffbba6",
+    repl = "#fca2a9",
+    grey = "#5c6370",
+    grey1 = "#4b5263",
+    gold = "#f5d070",
+    cyan = "#89ddff",
+    cyan2 = "#40d9ff",
+    pale = "#a2b8ee",
+    purple = "#c678dd",
+}
+return colours
