62050e2f03802f0d91bca4bd59e917a57f19bc63
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-03-16 09:15:34 +0100

    modify material

--- lua/config/colourschemes/material.lua
+++ lua/config/colourschemes/material.lua
@@ -10,6 +10,12 @@
 
 -- Styles: Oceanic, Deep ocean, Palenight, Lighter, Darker
 vim.g.material_style = 'deep ocean'
+local colors = require('material.colors')
+local cols = require('config.colourschemes.global')
+
+local function ll()
+    vim.notify("a")
+end
 
 -- Setup
 require('material').setup({
@@ -19,18 +25,42 @@ require('material').setup({
         cursor_line = true,
         non_current_windows = false
     },
+    plugins = {
+        "nvim-cmp", "nvim-tree",
+        "nvim-web-devicons", "gitsigns",
+        "telescope", "which-key",
+        "indent-blankline",
+    },
     styles = {
+        comments = {},
         strings = { italic = true },
         keywords = { bold = true },
-        functions = { bold = true }
-    },
-    plugins = {
-        "gitsigns", "nvim-cmp",
-        "telescope", "which-key",
-        "nvim-cmp", "nvim-web-devicons",
-        "indent-blankline"
+        functions = { bold = true },
+        variables = {},
+        operators = {},
+        types = {}
     },
     disable = {
         colored_cursor = true
-    }
+    },
+    custom_colors = function (colors)
+        colors.editor.bg = cols.bg
+        colors.editor.bg_alt = cols.bg1
+        colors.editor.fg = cols.fg1
+        colors.editor.fg_dark = cols.fg
+        colors.editor.line_numbers = cols.fg
+        colors.editor.accent = cols.blue
+        -- Override colors
+        colors.main.paleblue = cols.pale
+        colors.main.cyan = cols.gold
+        colors.main.yellow = cols.gold
+        colors.main.purple = cols.purple
+        -- Colours
+        colors.syntax.comments = cols.grey
+        colors.syntax.keyword = cols.purple
+        colors.syntax.fn = cols.blue
+        colors.syntax.type = cols.gold
+        colors.syntax.value = cols.red
+        colors.syntax.operator = cols.orange
+    end
 })
