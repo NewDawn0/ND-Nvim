46b6b240aaf51a76c1fa210ed561949dd9773d47
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-05-04 21:37:06 +0200

    add NvChad init.lua

--- init.lua
+++ init.lua
@@ -1,56 +1,20 @@
---[[
- _   _               ____                        ___
-| \ | | _____      _|  _ \  __ ___      ___ __  / _ \
-|  \| |/ _ \ \ /\ / / | | |/ _` \ \ /\ / / '_ \| | | |
-| |\  |  __/\ V  V /| |_| | (_| |\ V  V /| | | | |_| |
-|_| \_|\___| \_/\_/ |____/ \__,_| \_/\_/ |_| |_|\___/
------------  https://github.com/NewDawn0  -----------
---]]
+require "core"
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
--- Set termguicolors
-vim.opt.termguicolors = true
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.cmd('set tabstop=4')
-
--- Packages
-require('bootstrap')
-require('lazy').setup(require('plugins'), { ui = { border = "rounded" } })
-
--- Improve speed
-require('impatient')
-
--- Settings, commands, autocmds & mappings
-require('autocmds')
-require('settings')
-require('mappings')
-require('commands')
-
--- vars
-local g = vim.g
-
--- Setup lsp
-require('config.lsp')
-
--- Colourscheme
-vim.cmd('colorscheme tokyonight-night')
--- vim.cmd('colorscheme habamax')
-
--- Open nvim tree on startup
-local function open_nvim_tree(data)
-    local directory = vim.fn.isdirectory(data.file) == 1
-    if not directory then
-        return
-    end
-    vim.cmd.cd(data.file)
-    require("nvim-tree.api").tree.open()
+if custom_init_path then
+  dofile(custom_init_path)
 end
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
--- neovide options
-if vim.g.neovide then
-    g.neovide_confirm_quit = false
-    g.neovide_input_macos_alt_is_meta = true
-    g.neovide_cursor_animation_length = 0.05
-    g.neovide_cursor_trail_size = 0.5
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
 end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
