15cccffff0adf3a6a25275277c05f16eb94e0101
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-05-07 23:38:05 +0200

    fix formatting

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -16,15 +16,24 @@ local api = vim.api
 local augroup = api.nvim_create_augroup("LspFormatting", {})
 
 local sources = {
-    -- webdev stuff
-    b.formatting.deno_fmt,                                                    -- choosed deno for ts/js files cuz its very fast!
-    b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
-    -- Lua
-    b.formatting.stylua,
-    -- cpp
-    b.formatting.clang_format,
+    -- formatting
+    b.formatting.asmfmt,     -- assembly
+    b.formatting.beautysh,   -- shell
+    b.formatting.black,      -- python
+    b.formatting.cbfmt,      -- code blocks in md & org
+    b.formatting.clang_format, -- C, C#, C++, Java, Cuda, Protobuf
+    b.formatting.cmake_format, -- cmake
+    b.formatting.codespell,  -- typo correction in text files
+    b.formatting.deno_fmt,   -- JS, TS, Json
+    b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } },
+    b.formatting.reorder_python_imports,
+    b.formatting.rustfmt, -- Rust
+    b.formatting.stylua, -- Lua
+    b.formatting.taplo, -- TOML
+    b.formatting.yamlfmt, -- YAML,
+    -- Hover
+    b.hover.dictionary, -- Definition
 }
-
 null_ls.setup {
     debug = true,
     sources = sources,
