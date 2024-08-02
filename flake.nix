b3bf7c62f4f0d5d8a0dab9a15068f79b5dcfe608
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-08-02 21:19:59 +0200

    Add nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -1,18 +1,30 @@
+# _   _ ____        _   _       _
+# | \ | |  _ \      | \ | |_   _(_)_ __ ___
+# |  \| | | | |_____|  \| \ \ / / | '_ ` _ \
+# | |\  | |_| |_____| |\  |\ V /| | | | | | |
+# |_| \_|____/      |_| \_| \_/ |_|_| |_| |_|
+# Copyright: 2024 NewDawn0
+#
+# Author: NewDawn0 (https://github.com/NewDawn0)
+
 {
   description = "NewDawn0's NeoVim config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
     nix-systems.url = "github:nix-systems/default";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs:
     let
+      opts = import ./nix/opts.nix;
       eachSystem = nixpkgs.lib.genAttrs (import inputs.nix-systems);
-      mkPkgs = system:
-        import nixpkgs {
-          config = { allowUnfree = true; };
+      mkPkgs = system: {
+        unstable = nixpkgs-unstable.legacyPackages.${system};
+        pkgs = import nixpkgs {
           inherit system;
+          config = { allowUnfree = true; };
           overlays = [
             (final: prev: (import ./nix/pkgOverlays.nix { pkgs = prev; }))
             (final: prev: {
@@ -20,6 +32,7 @@
             })
           ];
         };
+      };
     in {
       overlays.default = (final: prev: { ndnvim = self.packages.default; });
       packages = eachSystem (sys:
