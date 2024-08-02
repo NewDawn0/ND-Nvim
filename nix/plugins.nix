4d35f2aa7cc5c9dbe7b08a1efa384137023112c4
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-08-02 21:22:21 +0200

    Update plugins.nix

--- nix/plugins.nix
+++ nix/plugins.nix
@@ -1,44 +1,57 @@
+# _   _ ____        _   _       _
+# | \ | |  _ \      | \ | |_   _(_)_ __ ___
+# |  \| | | | |_____|  \| \ \ / / | '_ ` _ \
+# | |\  | |_| |_____| |\  |\ V /| | | | | | |
+# |_| \_|____/      |_| \_| \_/ |_|_| |_| |_|
+# Copyright: 2024 NewDawn0
+#
+# Author: NewDawn0 (https://github.com/NewDawn0)
+
 { pkgs }:
-with pkgs.vimPlugins;
-[
+with pkgs.vimPlugins; [
   alpha-nvim
   boole-nvim
   bufferline-nvim
-  colorful-winsep-nvim
-  comment-nvim
-  flash-nvim
-  gen-nvim
-  gitsigns-nvim
-  indent-blankline-nvim
-  lualine-nvim
-  markdown-preview-nvim
-  mini-surround
-  nvim-autopairs
-  nvim-colorizer-lua
-  nvim-notify
-  nvim-treesitter.withAllGrammars
-  nvim-web-devicons
-  oil-nvim
-  plenary-nvim
-  telescope-nvim
-  todo-comments-nvim
-  tokyonight-nvim
-  trim-nvim
-  undotree-nvim
-  vim-markdown-toc
-  vim-tmux-navigator
-  wrapping-nvim
-  nvim-cmp
-  nvim-lspconfig
   cmp-buffer
   cmp-nvim-lsp
   cmp-nvim-lua
   cmp-path
   cmp_luasnip
   codeium-nvim
+  colorful-winsep-nvim
+  comment-nvim
+  conform-nvim
+  dressing-nvim
+  flash-nvim
   friendly-snippets
+  gen-nvim
+  gitsigns-nvim
+  indent-blankline-nvim
+  lsp-zero-nvim
   lspkind-nvim
+  lualine-nvim
   luasnip
+  markdown-preview-nvim
+  mini-surround
+  nvim-autopairs
+  nvim-cmp
+  nvim-colorizer-lua
+  nvim-lint
+  nvim-lspconfig
+  nvim-notify
+  nvim-treesitter-textobjects
+  nvim-treesitter.withAllGrammars
+  nvim-web-devicons
+  oil-nvim
+  plenary-nvim
+  rustaceanvim
+  telescope-nvim
+  todo-comments-nvim
+  tokyonight-nvim
+  trim-nvim
+  undotree-nvim
+  vim-markdown-toc
+  vim-mcfunction
+  vim-tmux-navigator
+  wrapping-nvim
 ]
-# Temporary plugins
-++ [ mason-nvim mason-lspconfig-nvim ]
