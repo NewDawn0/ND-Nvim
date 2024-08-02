d4f8b5328796f4b335750a429d229b9a362b0595
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-08-02 21:21:53 +0200

    Create opts.nix

---
+++ nix/opts.nix
@@ -0,0 +1,48 @@
+# _   _ ____        _   _       _
+# | \ | |  _ \      | \ | |_   _(_)_ __ ___
+# |  \| | | | |_____|  \| \ \ / / | '_ ` _ \
+# | |\  | |_| |_____| |\  |\ V /| | | | | | |
+# |_| \_|____/      |_| \_| \_/ |_|_| |_| |_|
+# Copyright: 2024 NewDawn0
+#
+# Author: NewDawn0 (https://github.com/NewDawn0)
+
+{
+  langs = {
+    all.enabled = false;
+    arduino.enabled = false;
+    assembly.enabled = true;
+    awk.enabled = false;
+    bash.enabled = true;
+    cpp.enabled = true;
+    css.enabled = true;
+    docker.enabled = true;
+    fennel.enabled = false;
+    fortran.enabled = false;
+    gleam.enabled = false;
+    go.enabled = true;
+    haskell.enabled = false;
+    html.enabled = true;
+    hydrogen.enabled = true;
+    java.enabled = true;
+    javascript.enabled = true;
+    json.enabled = true;
+    lua.enabled = true;
+    markdown.enabled = true; # TODO: test
+    matlab.enabled = false; # NOTE: Untested
+    mcfunction.enabled = false;
+    nginx.enabled = false; # NOTE: Untested
+    nix.enabled = true;
+    nushell.enabled = false;
+    ocaml.enabled = false;
+    perl.enabled = false;
+    protobuf.enabled = false;
+    python.enabled = true;
+    rust.enabled = true;
+    svelte.enabled = false;
+    toml.enabled = true;
+    typescript.enabled = false;
+    yaml.enabled = true;
+    zig.enabled = true;
+  };
+}
