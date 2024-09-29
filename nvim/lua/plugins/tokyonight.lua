146fec35faabc6acbce6e42577dd1627eea0267d
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-09-29 18:02:15 +0200

    mv tokyonight cfg

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -7,12 +7,6 @@
 --
 -- Author: NewDawn0 (https://github.com/NewDawn0)
 
--- Utility
-local tokyonight_theme = require "colours.tokyonight-cols"
-
--- Overwrite colors
-package.loaded["tokyonight.colors.moon"] = tokyonight_theme.colors
-
 -- Setup tokyonight
 require("tokyonight").setup {
   style = "moon",
